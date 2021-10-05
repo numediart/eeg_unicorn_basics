758e00272e0aea406a8b2fa99b20e0e4378611f7
VDelv <victor.delvigne@gmail.com>
2021-10-05 14:59:51 +0200

    add first script

---
+++ pylsl_intro.py
@@ -0,0 +1,55 @@
+
+
+import os
+import time
+import msvcrt
+import datetime
+
+from pylsl import StreamInlet, resolve_stream
+
+def main():
+    path_dir = 'save_dir'
+    save_update = 5
+    stream_timestamp = 1000
+    streams = resolve_stream()
+
+    inlet = StreamInlet(streams[0])
+
+    aborted = False
+    print_sample = False
+    saving = False
+
+    i = 0
+    tot = ''
+
+    while not aborted:
+        
+        sample, timestamp = inlet.pull_sample()
+        
+        if saving and i%save_update==0:
+            tot += str(timestamp)
+            tot += '\t'
+            tot += '\t'.join(str(e) for e in sample)
+            tot += '\n'
+
+        if print_sample:
+            print("Timestame %.3f \t num_sample=%d" %(timestamp, len(sample)))
+        if msvcrt.kbhit() and msvcrt.getch()[0] == 27:
+            aborted = True
+        if msvcrt.kbhit() and msvcrt.getch()[0] == 112:
+            print_sample = not print_sample
+        if (msvcrt.kbhit() and msvcrt.getch()[0] == 115) or i==stream_timestamp:
+            print('Start Signal Registration')
+            file_name = os.path.join(path_dir, 'sig_'+datetime.datetime.now().strftime("%d_%m_%Y__%H_%M_%S")+'.txt')
+            t = time.time()
+            saving = not saving
+
+        i += 1
+    if saving:
+        csv = open(file_name, 'w')
+        csv.write(tot)
+        csv.close()
+    print(time.time()-t)
+
+if __name__ == '__main__':
+    main()
---
+++ pylsl_plot.py
@@ -0,0 +1,55 @@
+
+
+
+import msvcrt
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pylsl import StreamInlet, resolve_stream
+
+duration = 4
+sampling_frequency = 250
+down_sampling_ratio = 10
+choosen_electrode = 1
+
+plt.style.use('dark_background')
+
+def main():
+    streams = resolve_stream()
+    inlet = StreamInlet(streams[0])
+
+    aborted = False
+
+    i = 0
+    j = 0
+    t = np.zeros((int(duration*sampling_frequency/down_sampling_ratio)))
+    y = np.zeros((int(duration*sampling_frequency/down_sampling_ratio)))
+    
+    while not aborted:
+        j = i//down_sampling_ratio
+        sample, timestamp = inlet.pull_sample()
+
+        if i%down_sampling_ratio==0:
+            if j < int(duration*sampling_frequency/down_sampling_ratio):
+                t[j] = timestamp
+                y[j] = sample[choosen_electrode]
+            else:
+                t = np.roll(t, -1)
+                y = np.roll(y, -1)
+                t[-1] = timestamp
+                y[-1] = sample[choosen_electrode]
+
+                std = np.std(y)
+
+                plt.axis([t[0]-1, t[1]+1, np.min(y)-std, np.max(y)+std])
+
+                plt.plot(t, y, 'o-', c='c')
+                plt.pause(0.05)
+        i += 1
+        if msvcrt.kbhit() and msvcrt.getch()[0] == 27:
+            aborted = True
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
